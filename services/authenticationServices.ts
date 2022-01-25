40168b488f81a6092e16c5dd8c02859a2d618cab
hfshan247 <hfshan247@gmail.com>
2022-01-25 20:42:02 +0500

    Firebase
    Fetch colors service
    Fetching Colors

--- services/authenticationServices.ts
+++ services/authenticationServices.ts
@@ -1,6 +1,6 @@
 import { db, app } from './../config/db';
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendPasswordResetEmail } from "firebase/auth";
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, set, get, child } from "firebase/database";
 
 export const signInService = (authObject:AuthObject, onSuccess?:any,onFailure?:any) => {
     const {email, password} = authObject;
@@ -92,4 +92,48 @@ export const registerUserService = (userObject:UserObject, onSuccess?:any, onFai
             }
             onFailure(error);
         }
+}
+
+export const addColorService = (color:Color, onSuccess?:any, onFailure?:any) => {
+    const branch = `/colors/${color.id}`
+    console.log('Branch: ', branch)
+    if (db) {
+        set(ref(db, branch), color)
+        .then(() => {
+        // Signed in
+        onSuccess();
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        onFailure(error);
+      });
+    } else {
+      const error:ErrorObject = {
+      message: 'Something went wrong while executing your request'
+    }
+    onFailure(error);
+  }
+}
+
+export const fetchColorsService = (color:Color, onSuccess?:any, onFailure?:any) => {
+  const branch = 'colors';
+  console.log('Branch: ', branch)
+  if (db) {
+    // const dbRef = ref(getDatabase());
+    get(child(db, `/${branch}`)).then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+  } else {
+    const error:ErrorObject = {
+    message: 'Something went wrong while executing your request'
+  }
+  onFailure(error);
+}
 }
