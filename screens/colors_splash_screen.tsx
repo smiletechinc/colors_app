5d3766817cfc50f02e8598059587361c59d3a439
Farhan-96 <farhan96fareed@gmail.com>
2022-10-20 11:41:30 +0500

    done all

--- screens/colors_splash_screen.tsx
+++ screens/colors_splash_screen.tsx
@@ -1,52 +1,59 @@
 import React, {useState, useEffect} from 'react';
-import { ActivityIndicator, View, StyleSheet, Image, Text, Alert } from 'react-native';
+import {
+  ActivityIndicator,
+  View,
+  StyleSheet,
+  Image,
+  Text,
+  Alert,
+} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import LogoImage from '../components/image_containers/LogoImage';
-import { styles } from './index';
-import { connect, useDispatch } from 'react-redux';
-import {updateColor, updateColors, updateReduxColors} from '../redux/action/colorAction';
-import { fetchColorsService } from './../services/colorsServices';
+import {styles} from './index';
+import {connect, useDispatch} from 'react-redux';
+import {
+  updateColor,
+  updateColors,
+  updateReduxColors,
+} from '../redux/action/colorAction';
+import {fetchColorsService} from './../services/colorsServices';
 
 type Props = {
-  navigation:any
-  userAuthentication: any,
-  updateColors: any,
-  updateReduxColors: any,
-}
+  navigation: any;
+  userAuthentication: any;
+  updateColors: any;
+  updateReduxColors: any;
+};
 
-const SplashScreen: React.FunctionComponent<Props> = (props) => {
-  
-  const {navigation, userAuthentication, updateColors, updateReduxColors} = props;
+const SplashScreen: React.FunctionComponent<Props> = props => {
+  const {navigation, userAuthentication, updateColors, updateReduxColors} =
+    props;
   const [animating, setAnimating] = useState(false);
 
-    const fetchColorsSuccessFailure = (colorsError) => {
+  const fetchColorsSuccessFailure = colorsError => {
     console.log('colorsError: ', colorsError);
     updateReduxColors([]);
     navigation.replace('HomeScreen');
+  };
 
-  }
-
-  const fetchColorsSuccess = (colorsData) => {
+  const fetchColorsSuccess = colorsData => {
     //1. Update redux for colors from firebase
     //2. Navigate to home
     console.log('colorsData:', Object.values(colorsData));
     const colors = Object.values(colorsData);
-    console.log("colors ", colors);
+    console.log('colors ', colors);
     updateReduxColors(colors);
     navigation.replace('HomeScreen');
-  }
-
-
+  };
 
   useEffect(() => {
     setTimeout(() => {
       setAnimating(true);
-      if(userAuthentication){
+      if (userAuthentication) {
         // updateColors()
-        fetchColorsService(fetchColorsSuccess, fetchColorsSuccessFailure)  //call reducrer action
-      }
-      else {
+        fetchColorsService(fetchColorsSuccess, fetchColorsSuccessFailure); //call reducrer action
+      } else {
         navigation.replace('LandingScreen');
       }
     }, 5000);
@@ -56,37 +63,40 @@ const SplashScreen: React.FunctionComponent<Props> = (props) => {
     <View style={styles.container}>
       <LogoImage />
       <Text style={styles.splashText}> ColorApp </Text>
-        { <ActivityIndicator
-        animating={animating}
-        color="#FFFFFF"
-        size="large"
-        style={styles.activityIndicator}
+      {
+        <ActivityIndicator
+          animating={animating}
+          color="#FFFFFF"
+          size="large"
+          style={styles.activityIndicator}
         />
-       }
+      }
     </View>
   );
 };
 
 const mapStateToProps = state => {
-  console.log("Redux user props are comes:", JSON.stringify(state.userlogin.userAuthenticate));
+  console.log(
+    'Redux user props are comes:',
+    JSON.stringify(state.userlogin.userAuthenticate),
+  );
   return {
-    userAuthentication: state.userlogin.userAuthenticate
-  }
-}
+    userAuthentication: state.userlogin.userAuthenticate,
+  };
+};
 
 const mapDispatchToProps = dispatch => {
-  console.log("Adding a value && Dispatch is Called");
+  console.log('Adding a value && Dispatch is Called');
   return {
     updateColors: () => {
       dispatch(updateColors());
     },
-    updateReduxColors: (updatedcolor) => {
+    updateReduxColors: updatedcolor => {
       dispatch(updateReduxColors(updatedcolor));
-    }
-  }
-}
+    },
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(SplashScreen);
 
 // export default SplashScreen;
-
